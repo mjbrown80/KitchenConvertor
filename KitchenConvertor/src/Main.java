3ef16f9dcfba89608827fe7905a32179c1b4e5ee
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-10-23 15:55:34 -0400

    update 2

--- KitchenConvertor/src/Main.java
+++ KitchenConvertor/src/Main.java
@@ -5,51 +5,68 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Kitchen Measurement Convertor");
+        boolean continueNewConversion = true;
 
-        System.out.println("Starting measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
-                "(3) cup      (4) ounce\n " +
-                "(5) pint     (6) gallon");
+        while (continueNewConversion) {
+            System.out.println("Welcome to the Kitchen Measurement Convertor");
 
-        int userStartingMeasurement = scanner.nextInt();
+            System.out.println("Starting measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
+                    "(3) cup      (4) ounce\n " +
+                    "(5) pint     (6) gallon");
 
-        System.out.println("To measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
-                "(3) cup      (4) ounce\n " +
-                "(5) pint     (6) gallon");
+            int userStartingMeasurement = scanner.nextInt();
 
-        int userEndingMeaurement = scanner.nextInt();
+            System.out.println("To measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
+                    "(3) cup      (4) ounce\n " +
+                    "(5) pint     (6) gallon");
 
-        System.out.println("PLease enter value:");
+            int userEndingMeaurement = scanner.nextInt();
 
-        int userValue = scanner.nextInt();
+            System.out.println("PLease enter value:");
 
-        double convertValue = convertMeasurement(userStartingMeasurement, userEndingMeaurement, userValue);
+            int userValue = scanner.nextInt();
 
-        System.out.println(userValue + " " + getMeasurementType(userStartingMeasurement) + " = " + convertValue + " " + getMeasurementType(userEndingMeaurement));
-    }
-    public static double convertMeasurement(int from, int to, double value){
-        if (from == 1 && to ==2){
-            return value / 3.0;
+            double convertValue = convertMeasurement(userStartingMeasurement, userEndingMeaurement, userValue);
+
+            System.out.println(userValue + " " + getMeasurementType(userStartingMeasurement) + " = " + convertValue + " " + getMeasurementType(userEndingMeaurement));
+
+        System.out.println("Would you like to convert another measurement? (1) yes or (2) no");
+        int choice = scanner.nextInt();
+
+        if (choice == 2){
+            continueNewConversion = false;
         }
-        return value;
+        System.out.println("Thank you for using the Kitchen convertor.");
+
+        }
     }
 
-    public static String getMeasurementType(int measurement){
-        switch (measurement){
-            case 1:
-                return "teaspoon";
-            case 2:
-                return "tablespoon";
-            case 3:
-                return "cup";
-            case 4:
-                return "ounces";
-            case 5:
-                return "pint";
-            case 6:
-                return "gallon";
+        public static double convertMeasurement ( int from, int to, double value){
+            if (from == 1 && to == 2) {
+                return value / 3;
+            }
+            if (from == 2 && to == 1) {
+                return value * 3;
+            }
+            return value;
         }
-        return "unknown";
-    }
+
+        public static String getMeasurementType ( int measurement){
+            switch (measurement) {
+                case 1:
+                    return "teaspoon";
+                case 2:
+                    return "tablespoon";
+                case 3:
+                    return "cup";
+                case 4:
+                    return "ounces";
+                case 5:
+                    return "pint";
+                case 6:
+                    return "gallon";
+            }
+            return "unknown";
+        }
 
 }
