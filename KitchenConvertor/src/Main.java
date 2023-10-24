8971b1758f5af0e12dd14cf9349a9e7ba9327cbc
Matt Brown <mattbrown@Matts-MacBook-Pro.local>
2023-10-24 14:07:29 -0400

    update5

--- KitchenConvertor/src/Main.java
+++ KitchenConvertor/src/Main.java
@@ -14,13 +14,15 @@ public class Main {
             System.out.print("\u001B[0m");
 
 
-            System.out.println("Starting measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
+            System.out.println("Starting measurement type:\n Select and press enter\n " +
+                    "(1) teaspoon (2) tablespoon\n " +
                     "(3) cup      (4) ounce\n " +
                     "(5) pint     (6) gallon");
 
             int userStartingMeasurement = scanner.nextInt();
 
-            System.out.println("To measurement type:\n Select and press enter\n (1) teaspoon (2) tablespoon\n " +
+            System.out.println("To measurement type:\n Select and press enter\n " +
+                    "(1) teaspoon (2) tablespoon\n " +
                     "(3) cup      (4) ounce\n " +
                     "(5) pint     (6) gallon");
 
@@ -32,7 +34,7 @@ public class Main {
 
             double convertValue = convertMeasurement(userStartingMeasurement, userEndingMeaurement, userValue);
 
-            System.out.println(userValue + " " + getMeasurementType(userStartingMeasurement) + " = " + String.format("%.1f",convertValue) + " " + getMeasurementType(userEndingMeaurement));
+            System.out.println(userValue + " " + getMeasurementType(userStartingMeasurement) + " = " + String.format("%.2f",convertValue) + " " + getMeasurementType(userEndingMeaurement));
 
         System.out.println("Would you like to convert another measurement? (1) yes or (2) no");
         int choice = scanner.nextInt();
@@ -52,6 +54,69 @@ public class Main {
             if (from == 2 && to == 1) {
                 return value * 3;
             }
+            if(from == 1 && to == 3){
+                return value / 48;
+            }
+            if (from == 3 && to == 1){
+                return value * 48;
+            }
+            if (from == 1 && to == 4){
+                return value / 6;
+            }
+            if (from == 4 && to == 1){
+                return value * 6;
+            }
+            if (from == 1 && to == 5){
+                return value / 96;
+            }
+            if (from == 5 && to == 1){
+                return value * 96;
+            }
+            if (from == 1 && to == 6){
+                return value / 768;
+            }
+            if (from == 6 && to == 1){
+                return value * 768;
+            }
+            if (from == 2 && to == 3) {
+                return value / 16.231;
+            }
+            if(from == 3 && to == 2){
+                return value * 16.231;
+            }
+            if (from == 2 && to == 4){
+                return value / 2;
+            }
+            if (from == 4 && to == 2){
+                return value * 2;
+            }
+            if (from == 2 && to == 5){
+                return value / 32;
+            }
+            if (from == 5 && to == 2){
+                return value * 32;
+            }
+            if (from == 3 && to == 4){
+                return value / 8.115;
+            }
+            if (from == 4 && to == 3){
+                return value * 8.115;
+            }
+            if (from == 3 && to == 5) {
+                return value / 1.972;
+            }
+            if(from == 5 && to == 3){
+                return value * 1.972;
+            }
+            if (from == 3 && to == 6){
+                return value / 15.772;
+            }
+            if (from == 6 && to == 3){
+                return value * 15.772;
+            }
+
+
+
             return value;
         }
 
